5d12b66d952d3dd60c866de3f57a35325b9a6713
Mellamangaba <115025825+Mellamangaba@users.noreply.github.com>
2026-03-08 21:48:07 -0400

    creacion de cola

--- Proyecto2_SO/src/proyecto2_so/Estructuras/Cola.java
+++ Proyecto2_SO/src/proyecto2_so/Estructuras/Cola.java
@@ -4,10 +4,65 @@
  */
 package proyecto2_so.Estructuras;
 
-/**
- *
- * @author gabri
- */
-public class Cola {
+public class Cola<T> {
+    private Nodo<T> frente; // El primero en la fila (head)
+    private Nodo<T> fin;    // El último en la fila (tail)
+    private int tamaño;
+
+    public Cola() {
+        this.frente = null;
+        this.fin = null;
+        this.tamaño = 0;
+    }
+
+    // Método para agregar un elemento al final de la cola (Enqueue)
+    public void encolar(T data) {
+        Nodo<T> nuevoNodo = new Nodo<>(data);
+        if (estaVacia()) {
+            frente = nuevoNodo;
+            fin = nuevoNodo;
+        } else {
+            fin.setNext(nuevoNodo);
+            fin = nuevoNodo;
+        }
+        tamaño++;
+    }
+
+    // Método para sacar y devolver el primer elemento de la cola (Dequeue)
+    public T desencolar() {
+        if (estaVacia()) {
+            return null; // O podrías lanzar una excepción
+        }
+        T data = frente.getData();
+        frente = frente.getNext();
+        
+        // Si al sacar el elemento la cola quedó vacía, el fin también es null
+        if (frente == null) {
+            fin = null;
+        }
+        tamaño--;
+        return data;
+    }
+
+    // Método para ver el primer elemento sin sacarlo (Peek)
+    public T verFrente() {
+        if (estaVacia()) {
+            return null;
+        }
+        return frente.getData();
+    }
+
+    public boolean estaVacia() {
+        return tamaño == 0;
+    }
+
+    public int tamaño() {
+        return tamaño;
+    }
     
+    public void limpiar() {
+        frente = null;
+        fin = null;
+        tamaño = 0;
+    }
 }
