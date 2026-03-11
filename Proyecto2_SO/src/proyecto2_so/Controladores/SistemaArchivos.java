7762b37a1675bcad3387e83fa61bc73be0f7ef4f
Mellamangaba <115025825+Mellamangaba@users.noreply.github.com>
2026-03-11 00:27:37 -0400

    Se crea sistema archivos

--- Proyecto2_SO/src/proyecto2_so/Controladores/SistemaArchivos.java
+++ Proyecto2_SO/src/proyecto2_so/Controladores/SistemaArchivos.java
@@ -4,10 +4,50 @@
  */
 package proyecto2_so.Controladores;
 
-/**
- *
- * @author gabri
- */
+import proyecto2_so.Modelos.Directorio;
+
 public class SistemaArchivos {
+    private Directorio raiz; 
+    private int totalBloques;
     
+    
+    private boolean[] mapaBits; 
+
+    // Constructor
+    public SistemaArchivos(int totalBloques) {
+        this.totalBloques = totalBloques;
+        this.mapaBits = new boolean[totalBloques];
+        
+       
+        for (int i = 0; i < totalBloques; i++) {
+            this.mapaBits[i] = true;
+        }
+        
+
+        this.raiz = new Directorio("Raiz", "admin");
+    }
+
+    // --- GETTERS ---
+    public Directorio getRaiz() {
+        return raiz;
+    }
+
+    public int getTotalBloques() {
+        return totalBloques;
+    }
+
+    public boolean[] getMapaBits() {
+        return mapaBits;
+    }
+    
+   
+    public int calcularBloquesLibres() {
+        int libres = 0;
+        for (int i = 0; i < totalBloques; i++) {
+            if (mapaBits[i]) {
+                libres++;
+            }
+        }
+        return libres;
+    }
 }
