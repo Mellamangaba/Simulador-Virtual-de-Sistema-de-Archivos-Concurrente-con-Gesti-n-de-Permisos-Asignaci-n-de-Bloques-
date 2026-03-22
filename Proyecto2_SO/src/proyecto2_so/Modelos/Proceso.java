bc244763aef1f56a252630c7ec3e86b7e7ddd8b9
Mellamangaba <115025825+Mellamangaba@users.noreply.github.com>
2026-03-22 00:39:35 -0400

    clase proceso

--- Proyecto2_SO/src/proyecto2_so/Modelos/Proceso.java
+++ Proyecto2_SO/src/proyecto2_so/Modelos/Proceso.java
@@ -5,46 +5,30 @@
 package proyecto2_so.Modelos;
 
 public class Proceso {
-    private static int contadorIds = 1; 
-    
-    private int id;
+    private String idProceso; 
     private String operacion; 
-    private String estado;    
-    private Archivo archivoRelacionado; 
-    
+    private int bloqueDestino; 
 
-    private String usuarioCreador;
-
-    public Proceso(String operacion, Archivo archivoRelacionado, String usuarioCreador) {
-        this.id = contadorIds++; 
+    public Proceso(String idProceso, String operacion, int bloqueDestino) {
+        this.idProceso = idProceso;
         this.operacion = operacion;
-        this.estado = "NUEVO"; 
-        this.archivoRelacionado = archivoRelacionado;
-        this.usuarioCreador = usuarioCreador;
+        this.bloqueDestino = bloqueDestino;
     }
 
-    // Getters y Setters
-    public int getId() {
-        return id;
+    public String getIdProceso() {
+        return idProceso;
     }
 
     public String getOperacion() {
         return operacion;
     }
 
-    public String getEstado() {
-        return estado;
+    public int getBloqueDestino() {
+        return bloqueDestino;
     }
 
-    public void setEstado(String estado) {
-        this.estado = estado; 
-    }
-
-    public Archivo getArchivoRelacionado() {
-        return archivoRelacionado;
-    }
-
-    public String getUsuarioCreador() {
-        return usuarioCreador;
+    @Override
+    public String toString() {
+        return idProceso + " | " + operacion + " -> Ir al Bloque: " + bloqueDestino;
     }
 }
